4e7dfd4331bcd8b00addbdc9fc090cc3d94b8d24
Gustavo Londono <gCorrea@buildingengines.com>
2020-09-07 15:16:31 -0500

    Create pokemon Loader protocol

---
+++ PokedexFeature/PokedexFeature/Feature/PokemonLoader.swift
@@ -0,0 +1,15 @@
+//
+//  PokemonLoader.swift
+//  PokedexFeature
+//
+//  Created by Gustavo Londono on 9/7/20.
+//  Copyright © 2020 Gustavo Londono. All rights reserved.
+//
+
+import Foundation
+
+public protocol PokemonLoader {
+    typealias Result = Swift.Result<[Pokemon], Error>
+    
+    func load(completion: (Result) -> Void)
+}
