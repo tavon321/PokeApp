1c1ff40ae11c842078270cc4efec404d25f940b8
Gustavo Londono <gCorrea@buildingengines.com>
2020-09-07 15:09:34 -0500

    Create type model

---
+++ PokedexFeature/PokedexFeature/Feature/Model/Type.swift
@@ -0,0 +1,14 @@
+//
+//  Type.swift
+//  PokedexFeature
+//
+//  Created by Gustavo Londono on 9/7/20.
+//  Copyright © 2020 Gustavo Londono. All rights reserved.
+//
+
+import Foundation
+
+struct Type {
+    var name: String
+    var url: URL
+}
