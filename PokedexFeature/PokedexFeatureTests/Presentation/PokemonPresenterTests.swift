38d75489e2f49f26baa4868786cf2f88df870364
Gustavo Londono <gCorrea@buildingengines.com>
2020-09-07 17:44:30 -0500

    Group API Test into a folder

---
+++ PokedexFeature/PokedexFeatureTests/Presentation/PokemonPresenterTests.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonPresenterTests.swift
+//  PokedexFeatureTests
+//
+//  Created by Gustavo Londono on 9/7/20.
+//  Copyright © 2020 Gustavo Londono. All rights reserved.
+//
+
+import XCTest
+
+class PokemonPresenterTests: XCTestCase {
+
+}
