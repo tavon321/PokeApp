7769e5aece32e7d42309d8422f441114ceb0b23c
Gustavo Londono <gCorrea@buildingengines.com>
2020-09-07 15:08:43 -0500

    Create pokemon feature framework to speed testing without simulators.

---
+++ PokedexFeature/PokedexFeature/Feature/Model/Pokemon.swift
@@ -0,0 +1,15 @@
+//
+//  Pokemon.swift
+//  PokedexFeature
+//
+//  Created by Gustavo Londono on 9/7/20.
+//  Copyright © 2020 Gustavo Londono. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon {
+    var id: UUID
+    var name: String
+    var type: Type
+}
---
+++ PokedexFeature/PokedexFeature/Feature/PokedexFeature.h
@@ -0,0 +1,19 @@
+//
+//  PokedexFeature.h
+//  PokedexFeature
+//
+//  Created by Gustavo Londono on 9/7/20.
+//  Copyright © 2020 Gustavo Londono. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PokedexFeature.
+FOUNDATION_EXPORT double PokedexFeatureVersionNumber;
+
+//! Project version string for PokedexFeature.
+FOUNDATION_EXPORT const unsigned char PokedexFeatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PokedexFeature/PublicHeader.h>
+
+
