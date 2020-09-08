d97a2de1bf1245a131e2808558f57d9498dc173f
Gustavo Londono <gCorrea@buildingengines.com>
2020-09-07 19:32:25 -0500

    Add pokeDexApp

---
+++ PokeDexApp/PokeDexApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PokeDexApp
+//
+//  Created by Gustavo Londono on 9/7/20.
+//  Copyright © 2020 Gustavo Londono. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
