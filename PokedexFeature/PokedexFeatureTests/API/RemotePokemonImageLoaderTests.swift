24f12effa325dfb6d9b248d937f6ff60955443df
Gustavo Londono <gCorrea@buildingengines.com>
2020-09-09 10:29:32 -0500

    Load image delivers invalid data error with client response with empty data.

--- PokedexFeature/PokedexFeatureTests/API/RemotePokemonImageLoaderTests.swift
+++ PokedexFeature/PokedexFeatureTests/API/RemotePokemonImageLoaderTests.swift
@@ -48,14 +48,14 @@ class RemotePokemonImageLoaderTests: XCTestCase {
         }
     }
     
-//    func test_loadImageDataFromURL_deliversInvalidDataErrorOn200HTTPResponseWithEmptyData() {
-//        let (sut, client) = makeSUT()
-//        
-//        expect(sut, toCompleteWith: failure(.invalidData), when: {
-//            let emptyData = Data()
-//            client.complete(withStatusCode: 200, data: emptyData)
-//        })
-//    }
+    func test_loadImageDataFromURL_deliversInvalidDataErrorOn200HTTPResponseWithEmptyData() {
+        let (sut, client) = createSUT(url: anyURL)
+
+        expect(sut: sut, toCompleteWith: .failure(.invalidData), when: {
+            let emptyData = Data()
+            client.complete(withStatusCode: 200, data: emptyData)
+        })
+    }
     
     // MARK: Helpers
     private func expect(sut: RemotePokemonImageLoader,
